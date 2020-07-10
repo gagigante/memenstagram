b6f66c8b6c23818dbe5db7a0238b6362a0a1d027
Gabriel Gigante <gabriel_gigante@outlook.com>
2020-07-10 20:31:07 -0300

    Config typescript and paths

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,8 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'Hello world' });
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello world' });
 });
 
 app.listen(3333);
