c5750ff8f1b321aea6cfe99c301c07f1e0ed87e1
Gabriel Gigante <gabriel_gigante@outlook.com>
2020-09-03 20:55:30 -0300

    Update user avatar feature

--- mobile/src/pages/EditProfile/index.tsx
+++ mobile/src/pages/EditProfile/index.tsx
@@ -1,45 +1,174 @@
-import React from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import React, {useCallback, useRef} from 'react';
+import {
+  Platform,
+  Alert,
+  KeyboardAvoidingView,
+  ScrollView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+
+import ImagePicker from 'react-native-image-picker';
+
+import Icon from 'react-native-vector-icons/Feather';
+
+import {Form} from '@unform/mobile';
+import {FormHandles} from '@unform/core';
+
+import {useNavigation} from '@react-navigation/native';
 
 import {useAuth} from '../../hooks/auth';
 
-// import { Container } from './styles';
+import api from '../../services/api';
+
+import SignInput from '../../components/SignInput';
+
+import {
+  stylesheet,
+  Container,
+  UserAvatarButton,
+  UserAvatar,
+  UserAvatarIconView,
+  Divider,
+} from './styles';
+
+// import AvatarPlaceholder from '../../assets/avatar-placeholder.png';
 
 const EditProfile: React.FC = () => {
-  const {signOut} = useAuth();
+  const {user, signOut, updateUser} = useAuth();
+  const {navigate} = useNavigation();
 
-  // const handleUpdateAvatar = useCallback(() => {
-  //   ImagePicker.showImagePicker(
-  //     {
-  //       title: 'Selecione uma foto de perfil',
-  //       cancelButtonTitle: 'Cancelar',
-  //       takePhotoButtonTitle: 'Usar câmera',
-  //       chooseFromLibraryButtonTitle: 'Escolher da galeria',
-  //     },
-  //     (response) => {
-  //       if (response.didCancel) {
-  //         return;
-  //       }
+  const formRef = useRef<FormHandles>(null);
+  const nicknameInputRef = useRef<TextInput>(null);
+  const emailInputRef = useRef<TextInput>(null);
+  const passwordInputRef = useRef<TextInput>(null);
+  const passwordConfirmationInputRef = useRef<TextInput>(null);
 
-  //       if (response.error) {
-  //         Alert.alert(response.error);
-  //         return;
-  //       }
+  const handleUpdateAvatar = useCallback(() => {
+    ImagePicker.showImagePicker(
+      {
+        title: 'Selecione uma foto de perfil',
+        cancelButtonTitle: 'Cancelar',
+        takePhotoButtonTitle: 'Usar câmera',
+        chooseFromLibraryButtonTitle: 'Escolher da galeria',
+      },
+      async (response) => {
+        if (response.didCancel) {
+          return;
+        }
 
-  //       setUserAvatar({
-  //         type: 'image/jpeg',
-  //         uri: response.uri,
-  //       });
-  //     },
-  //   );
-  // }, []);
+        if (response.error) {
+          Alert.alert('Erro ao atualizar avatar');
+          return;
+        }
+
+        const data = new FormData();
+
+        data.append('avatar', {
+          type: 'image/jpeg',
+          name: `${user.id}.jpg`,
+          uri: response.uri,
+        });
+        console.log('esse é o data: ' + data);
+        const res = await api.patch('/users/avatar', data);
+
+        updateUser(res.data);
+      },
+    );
+  }, [updateUser, user.id]);
 
   return (
-    <View>
-      <TouchableOpacity onPress={signOut}>
-        <Text>Sair</Text>
-      </TouchableOpacity>
-    </View>
+    <KeyboardAvoidingView
+      style={stylesheet.KeyboardAvoidingViewStyle}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      enabled>
+      <ScrollView
+        contentContainerStyle={stylesheet.scrollViewStyle}
+        keyboardShouldPersistTaps="handled">
+        <Container>
+          <UserAvatarButton onPress={handleUpdateAvatar}>
+            <UserAvatar source={{uri: user.avatar_url}} />
+
+            <UserAvatarIconView>
+              <Icon name="camera" color="#fff" size={24} />
+            </UserAvatarIconView>
+          </UserAvatarButton>
+
+          <Form ref={formRef} onSubmit={() => console.log('Entrou')}>
+            <SignInput
+              name="name"
+              placeholder="Name"
+              autoCorrect={false}
+              autoCapitalize="words"
+              returnKeyType="next"
+              onSubmitEditing={() => {
+                nicknameInputRef.current?.focus();
+              }}
+            />
+
+            <SignInput
+              ref={nicknameInputRef}
+              name="nickname"
+              placeholder="Nickname"
+              autoCorrect={false}
+              autoCapitalize="none"
+              returnKeyType="next"
+              onSubmitEditing={() => {
+                emailInputRef.current?.focus();
+              }}
+            />
+
+            <SignInput
+              ref={emailInputRef}
+              name="email"
+              placeholder="E-mail"
+              autoCorrect={false}
+              autoCapitalize="none"
+              keyboardType="email-address"
+              returnKeyType="next"
+              onSubmitEditing={() => {
+                passwordInputRef.current?.focus();
+              }}
+            />
+
+            <Divider />
+
+            <SignInput
+              ref={passwordInputRef}
+              name="password"
+              passwordInput
+              placeholder="Password"
+              autoCapitalize="none"
+              returnKeyType="next"
+              onSubmitEditing={() => {
+                passwordConfirmationInputRef.current?.focus();
+              }}
+            />
+
+            <SignInput
+              ref={passwordConfirmationInputRef}
+              name="password_confirmation"
+              passwordInput
+              placeholder="Password confirmation"
+              autoCapitalize="none"
+              returnKeyType="send"
+              onSubmitEditing={() => {
+                formRef.current?.submitForm();
+              }}
+            />
+          </Form>
+
+          <TouchableOpacity onPress={signOut}>
+            <Text>Sair</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => navigate('LoadingPage')}>
+            <Text>navigate</Text>
+          </TouchableOpacity>
+        </Container>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
--- mobile/src/pages/EditProfile/styles.ts
+++ mobile/src/pages/EditProfile/styles.ts
@@ -1,32 +1,54 @@
 import styled from 'styled-components/native';
+import {StyleSheet} from 'react-native';
 
-export const Container = styled.View``;
+export const stylesheet = StyleSheet.create({
+  KeyboardAvoidingViewStyle: {
+    position: 'relative',
+    flex: 1,
+  },
 
-// export const UserAvatarButton = styled.TouchableOpacity`
-//   position: relative;
-//   width: 186px;
-//   height: 186px;
-//   margin-top: 42px;
-//   margin-bottom: 32px;
-//   align-self: center;
-// `;
+  scrollViewStyle: {
+    paddingVertical: 16,
+    position: 'relative',
+  },
+});
 
-// export const UserAvatar = styled.Image`
-//   width: 186px;
-//   height: 186px;
-//   border-radius: 98px;
-//   align-self: center;
-// `;
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+`;
 
-// export const UserAvatarIconView = styled.View`
-//   position: absolute;
-//   justify-content: center;
-//   align-items: center;
-//   width: 54px;
-//   height: 54px;
-//   border-radius: 27px;
-//   background-color: #333333;
-//   right: 4%;
-//   bottom: 4%;
-//   z-index: 5;
-// `;
+export const UserAvatarButton = styled.TouchableOpacity`
+  position: relative;
+  width: 186px;
+  height: 186px;
+  margin-top: 42px;
+  margin-bottom: 32px;
+  align-self: center;
+`;
+
+export const UserAvatar = styled.Image`
+  width: 186px;
+  height: 186px;
+  border-radius: 98px;
+  align-self: center;
+`;
+
+export const UserAvatarIconView = styled.View`
+  position: absolute;
+  justify-content: center;
+  align-items: center;
+  width: 54px;
+  height: 54px;
+  border-radius: 27px;
+  background-color: #333333;
+  right: 4%;
+  bottom: 4%;
+  z-index: 5;
+`;
+
+export const Divider = styled.View`
+  background-color: #e6e6f0;
+  margin: 12px 8%;
+  height: 1px;
+`;
