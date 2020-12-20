df3989cf0f1af713091beb727d2c81c551dc6bf3
Gabriel Gigante <gabriel_gigante@outlook.com>
2020-12-19 22:58:51 -0300

    Draft: delete user feature

--- backend/src/modules/users/infra/http/controllers/AccountController.ts
+++ backend/src/modules/users/infra/http/controllers/AccountController.ts
@@ -3,6 +3,7 @@ import { container } from 'tsyringe';
 import { classToClass } from 'class-transformer';
 
 import ActivateAccountService from '@modules/users/services/ActivateAccountService';
+// import DeleteAccountService from '@modules/users/services/DeleteAccountService';
 
 export default class AccountController {
   public async update(request: Request, response: Response): Promise<Response> {
@@ -18,4 +19,17 @@ export default class AccountController {
 
     return response.json(classToClass(updatedUser));
   }
+
+  /*   public async destroy(
+    request: Request,
+    response: Response,
+  ): Promise<Response> {
+    const loggedUserId = request.user.id;
+
+    const deleteAccount = container.resolve(DeleteAccountService);
+
+    await deleteAccount.execute({ loggedUserId });
+
+    return response.status(204).json();
+  } */
 }
