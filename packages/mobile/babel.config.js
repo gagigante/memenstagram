0af8be3a3cc8be7e34d50c3db29be0b26036b769
Gabriel Gigante <gabriel_gigante@outlook.com>
2021-05-16 00:02:18 -0300

    feat: setup typescript paths for react-native project

--- packages/mobile/babel.config.js
+++ packages/mobile/babel.config.js
@@ -1,3 +1,29 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ts',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@hooks': './src/hooks',
+          '@models': './src/models',
+          '@pages': './src/pages',
+          '@routes': './src/routes',
+          '@services': './src/services',
+          '@styles': './src/styles',
+          '@utils': './src/utils'
+        },
+      },
+    ],
+  ],
 };
